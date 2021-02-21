8bdc8562ad1ff823dec114c1001ecd24002c36ab
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-02-21 18:19:03 +0100

    Add VooDo.Path

--- VooDo.WinUI/Source/XAML/VooDo.cs
+++ VooDo.WinUI/Source/XAML/VooDo.cs
@@ -2,10 +2,13 @@
 using Microsoft.UI.Xaml.Markup;
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
 using VooDo.Runtime;
+using VooDo.Utils;
 using VooDo.WinUI.Core;
 
 namespace VooDo.WinUI.Xaml
@@ -23,18 +26,36 @@ namespace VooDo.WinUI.Xaml
         }
 
         public string? Code { get; set; }
+        public string? Path { get; set; }
 
         public Binding? Binding { get; private set; }
         private object? m_lastValue;
 
+        private static readonly Dictionary<string, string> s_codeCache = new Dictionary<string, string>();
+
+        private static string GetCode(string _path)
+        {
+            _path = NormalizeFilePath.Normalize(_path);
+            if (!s_codeCache.TryGetValue(_path, out string? code))
+            {
+                s_codeCache[_path] = code = File.ReadAllText(_path);
+            }
+            return code;
+        }
+
         protected override object? ProvideValue(IXamlServiceProvider _serviceProvider)
         {
+            if (Code is null && Path is null)
+            {
+                throw new InvalidOperationException("No code or path specified");
+            }
+            if (Code is not null && Path is not null)
+            {
+                throw new InvalidOperationException("Cannot specify both Code and Path");
+            }
             if (Binding is null)
             {
-                if (Code is null)
-                {
-                    throw new InvalidOperationException("Code is null");
-                }
+                string code = Code ?? GetCode(Path!);
                 IProvideValueTarget? provideValueTarget = (IProvideValueTarget?) _serviceProvider.GetService(typeof(IProvideValueTarget));
                 IRootObjectProvider? rootObjectProvider = (IRootObjectProvider?) _serviceProvider.GetService(typeof(IRootObjectProvider));
                 IUriContext? uriContext = (IUriContext?) _serviceProvider.GetService(typeof(IUriContext));
@@ -46,7 +67,7 @@ namespace VooDo.WinUI.Xaml
                 {
                     throw new InvalidOperationException("Failed to retrieve XAML target property");
                 }
-                XamlInfo xamlInfo = new XamlInfo(Code, property, provideValueTarget.TargetObject, rootObjectProvider.RootObject, uriContext.BaseUri);
+                XamlInfo xamlInfo = new XamlInfo(code, property, provideValueTarget.TargetObject, rootObjectProvider.RootObject, uriContext.BaseUri);
                 Binding = BindingManager.AddBinding(xamlInfo);
                 if (Binding.Program is TypedProgram typedProgram)
                 {
