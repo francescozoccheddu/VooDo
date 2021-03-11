fae058394a5e121ccee22b335fe315a0331e8ceb
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2021-03-11 20:23:52 +0100

    Add CustomGridView

---
+++ Testbench/Desktop/Desktop/CustomGridView.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace VooDo.Testbench.Desktop
+{
+    internal sealed class CustomGridView : GridView
+    {
+
+        public ScrollViewer ScrollViewer { get; private set; } = null!;
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            ScrollViewer = (ScrollViewer)GetTemplateChild("ScrollViewer");
+        }
+    }
+}
--- Testbench/Desktop/Desktop/MainWindow.xaml.cs
+++ Testbench/Desktop/Desktop/MainWindow.xaml.cs
@@ -9,12 +9,13 @@ namespace VooDo.Testbench.Desktop
     public sealed partial class MainWindow : Window
     {
 
+
         public MainWindow()
         {
             InitializeComponent();
             Activated += MainWindow_Activated;
             Closed += MainWindow_Closed;
-            m_gridView.ItemsSource = Enumerable.Repeat(new Item() { Title = "Title", Price = 6.99f }, 10).ToArray();
+            m_gridView.ItemsSource = Enumerable.Repeat(new Item() { Title = "Title", Price = 6.99f }, 100).ToArray();
         }
 
         private void MainWindow_Closed(object _sender, WindowEventArgs _args) => Binder.Unbind(this);
